e90ffb1fe28333d42beb2e0e85b8cda16b60fe6d
Mads Kristensen <post@madskristensen.net>
2016-08-08 07:57:08 -0700

    Added background loading of package

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -5,10 +5,11 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace BrowserReloadOnSave
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionHasSingleProject, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionHasMultipleProjects, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideOptionPage(typeof(Options), "Web", Vsix.Name, 101, 102, true, new string[0], ProvidesLocalizedCategoryName = false)]
     [Guid(PackageGuids.guidBrowserReloadPackageString)]
     public sealed class VSPackage : Package
