5c4848f9abe6a7b428188e4143e550a508317827
Mads Kristensen <post@madskristensen.net>
2016-09-07 12:26:56 -0700

    Only load package in ASP.NET/Web projects

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace BrowserReloadOnSave
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionHasSingleProject, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionHasMultipleProjects, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideOptionPage(typeof(Options), "Web", Vsix.Name, 101, 102, true, new string[0], ProvidesLocalizedCategoryName = false)]
+    [ProvideAutoLoad("{349C5852-65DF-11dA-9384-00065B846F21}", PackageAutoLoadFlags.BackgroundLoad)] // WAP
+    [ProvideAutoLoad("{E24C65DC-7377-472b-9ABA-BC803B73C61A}", PackageAutoLoadFlags.BackgroundLoad)] // WebSite
+    [ProvideAutoLoad("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}", PackageAutoLoadFlags.BackgroundLoad)] // ProjectK
     [Guid(PackageGuids.guidBrowserReloadPackageString)]
     public sealed class VSPackage : Package
     {
-        public static Options Options;
+        public static Options Options
+        {
+            get;
+            private set;
+        }
 
         protected override void Initialize()
         {
@@ -22,8 +26,6 @@ namespace BrowserReloadOnSave
 
             Logger.Initialize(this, Vsix.Name);
             EnableReloadCommand.Initialize(this);
-
-            base.Initialize();
         }
     }
 }
